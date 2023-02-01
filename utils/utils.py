49581679fd901ae7b3970556fdde4dbdb8ed7fff
Michele Ronco <ronco.michele@yahoo.it>
2023-02-01 17:46:35 +0100

    added notebook on forest detection

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import itertools
 
 def r_squared(y, y_hat):
 
@@ -156,3 +157,35 @@ class PreProc():
             out[:,:,:,i] = (img[:,:,:,i] - img[:,:,:,i].min())/(img[:,:,:,i].max()-img[:,:,:,i].min())
         
         return out
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues, ylabel='True label', xlabel='Predicted label', filename=None):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    np.set_printoptions(precision=2, suppress=True)
+    
+    if normalize:
+        cm = cm.astype('float') / (cm.sum(axis=1)[:, np.newaxis] + np.finfo(np.float).eps)
+    
+    plt.imshow(cm, interpolation='nearest', cmap=cmap, vmin=0, vmax=1)
+    plt.title(title, fontsize=20)
+    # plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45, fontsize=20)
+    plt.yticks(tick_marks, classes, fontsize=20)
+    
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black",
+                 fontsize=12)
+
+    plt.tight_layout()
+    plt.ylabel(ylabel, fontsize=20)
+    plt.xlabel(xlabel, fontsize=20)
